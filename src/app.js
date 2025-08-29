1e970a548d037e9331d0fd56021b3d137b1a68f1
pravesh1731 <706890pk@gmail.com>
2025-08-29 09:04:13 +0530

    created a express server

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+
+app.use("/home",(req, res) => {
+res.send("this ia the home page");
+}); 
+app.use("/profile",(req, res) => {
+res.send("welcome this is your profile");
+});
+app.use("/menu",(req, res) => {
+res.send("MENU MENU is comming soon....");
+});
+app.use("/ab",(req, res) => {
+res.send("ab ab ab ab.");
+});
+
+
+app.listen(3001, ()=>{
+    console.log("Server is running on port 3001");
+})
