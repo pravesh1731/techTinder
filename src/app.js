d2302d61799f04fa00928bd17040714d37dffb69
pravesh1731 <706890pk@gmail.com>
2025-08-30 10:31:09 +0530

    exploring get post delete

--- src/app.js
+++ src/app.js
@@ -5,15 +5,28 @@ const app = express();
 app.use("/home",(req, res) => {
 res.send("this ia the home page");
 }); 
+app.get("/user",(req, res) => {
+res.send({ first_name : "Pravesh", last_name : "Chaudhary"});
+});
+app.post("/user",(req, res) => {
+res.send("Data save Successfully to the Database");
+});
+app.delete("/user",(req, res) => {
+res.send("Data delete Successfully from the Database");
+});
 app.use("/profile",(req, res) => {
 res.send("welcome this is your profile");
 });
 app.use("/menu",(req, res) => {
 res.send("MENU MENU is comming soon....");
 });
+app.use("/ab/2",(req, res) => {
+res.send("ab ab.");
+});
 app.use("/ab",(req, res) => {
 res.send("ab ab ab ab.");
 });
+
 
 
 app.listen(3001, ()=>{
