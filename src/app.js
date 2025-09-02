40df614d2a31ba928451af793c529d44a91de1fb
pravesh1731 <706890pk@gmail.com>
2025-09-02 09:16:07 +0530

    use of next

--- src/app.js
+++ src/app.js
@@ -1,32 +1,31 @@
 const express = require("express");
 const app = express();
 
+//order matter alot
+//app.use("/user", rH1, rH2, [rH3, rH4], rH5)
 
-app.use("/home",(req, res) => {
-res.send("this ia the home page");
-}); 
-app.get("/user",(req, res) => {
-res.send({ first_name : "Pravesh", last_name : "Chaudhary"});
-});
-app.post("/user",(req, res) => {
-res.send("Data save Successfully to the Database");
-});
-app.delete("/user",(req, res) => {
-res.send("Data delete Successfully from the Database");
-});
-app.use("/profile",(req, res) => {
-res.send("welcome this is your profile");
-});
-app.use("/menu",(req, res) => {
-res.send("MENU MENU is comming soon....");
-});
-app.use("/ab/2",(req, res) => {
-res.send("ab ab.");
-});
-app.use("/ab",(req, res) => {
-res.send("ab ab ab ab.");
-});
-
+app.get("/user", (req, res, next)=>{
+    console.log("Response 1");
+    // res.send("User response 1");
+    next();
+},
+(req, res, next)=>{
+    console.log("Response 2");
+    res.send("User response 2");
+},
+(req, res, next)=>{
+    console.log("Response 3");
+    res.send("User response 3");
+},
+(req, res, next)=>{
+    console.log("Response 4");
+    res.send("User response 4");
+},
+(req, res, next)=>{
+    console.log("Response 5");
+    res.send("User response 5");
+},
+)
 
 
 app.listen(3001, ()=>{
